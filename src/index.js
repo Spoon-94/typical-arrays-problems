4b625d0153c9d6faf26912f6112f2ace628d27ec
Dima Paymushkin <Dimon-94@yandex.ru>
2020-10-06 07:21:35 +0400

    feat: complete task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+exports.min = function min(array) {
+    if (array && array.length) {
+        return Math.min(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array && array.length) {
+        return Math.max(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array && array.length) {
+        return array.reduce((acc, cur) => acc + cur) / array.length;
+    } else {
+        return 0;
+    }
+};
